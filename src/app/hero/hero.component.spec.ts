fb7aa5a59aa1f107edda03e5d36578e311b14f4b
ramazan.alp <ramazan.alp@pia-team.com>
2021-10-19 15:27:40 +0100

    testing template

--- src/app/hero/hero.component.spec.ts
+++ src/app/hero/hero.component.spec.ts
@@ -1,10 +1,28 @@
-import { TestBed } from "@angular/core/testing";
-import { HeroComponent } from "./hero.component";
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HeroComponent } from './hero.component';
 
-describe("HeroComponent", () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [HeroComponent],
-    });
-  });
+describe('HeroComponent', () => {
+	let fixture: ComponentFixture<HeroComponent>;
+	beforeEach(() => {
+		TestBed.configureTestingModule({
+			declarations: [HeroComponent],
+			schemas: [NO_ERRORS_SCHEMA],
+		});
+		fixture = TestBed.createComponent(HeroComponent);
+	});
+
+	it('should have correct hero', () => {
+		fixture.componentInstance.hero = { id: 1, name: 'SuperDude', strength: 3 };
+		//expect(fixture.componentInstance.hero).toEqual('SuperDude');
+	});
+
+	it('Should render the hero name in anchor tag', () => {
+		fixture.componentInstance.hero = { id: 1, name: 'SuperDude', strength: 3 };
+
+		fixture.detectChanges();
+		expect(fixture.nativeElement.querySelector('a').textContent).toContain(
+			'SuperDude'
+		);
+	});
 });
