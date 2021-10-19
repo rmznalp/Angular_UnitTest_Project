52090ffa36d3f1d3b7879746641b30b2dc949d79
ramazan.alp <ramazan.alp@pia-team.com>
2021-10-19 09:33:23 +0100

    adding some tests

---
+++ src/app/hero/hero.component.spec.ts
@@ -0,0 +1,10 @@
+import { TestBed } from "@angular/core/testing";
+import { HeroComponent } from "./hero.component";
+
+describe("HeroComponent", () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeroComponent],
+    });
+  });
+});
